66f6d44d9149d19ea9925fa57d83f3dd35e5dfe0
FA77\flavi <flavioalex19@gmail.com>
2024-09-10 19:09:21 -0300

    Draft logic WIP

--- Assets/Scripts/Managers/DraftManager.cs
+++ Assets/Scripts/Managers/DraftManager.cs
@@ -10,7 +10,13 @@ public class DraftManager : MonoBehaviour
 
     [SerializeField] GameObject p_leaguePlayer; // Player prefab
     [SerializeField] int numberOfPlayerToGenerate;//Numbers of player to create- this number will change if this is being called at the new season o a continiation of one
-    [SerializeField] List<Player> p_DraftPlayerList = new List<Player>();
+    [SerializeField] List<Player> p_DraftPlayerList = new List<Player>();//Avaliable for the draft
+
+    // Step 1: Declare the dictionary to store player-button associations
+    Dictionary<Player, GameObject> playerButtonMap = new Dictionary<Player, GameObject>();
+
+    [SerializeField] List<Team> t_Teams = new List<Team>();
+    int t_currentTeamIndex = 0;
 
     [SerializeField] GridLayoutGroup ui_viewport;
 
@@ -29,6 +35,7 @@ public class DraftManager : MonoBehaviour
 
     void Start()
     {
+        print(p_DraftPlayerList.Count + " Avaliable Players");
         GeneratePlayers();
     }
 
@@ -49,10 +56,7 @@ public class DraftManager : MonoBehaviour
 
             }
         }
-    }
-    public void AddPlayerToDraftList(GameObject player)
-    {
-
+        print(p_DraftPlayerList.Count + " Avaliable Players");
     }
     //later remove this from draft manager and put on the game manager sincve that you need this to see avalible player after the draft and the team rooster
     void CreatePlayerButton(Player player)
@@ -61,11 +65,53 @@ public class DraftManager : MonoBehaviour
         GameObject newButton = Instantiate(player.GetPlayerInfoButton());
         // Set the button's parent to the grid layout group
         newButton.transform.SetParent(ui_viewport.transform, false);
+
+        // Step 2: Track the player-button association
+        playerButtonMap[player] = newButton;
+
         // Find the Text component that will display the OVR
         TextMeshProUGUI ovrText = newButton.transform.Find("Text_Draft_PlayerOVR").GetComponent<TextMeshProUGUI>();
         if (ovrText != null)
         {
             ovrText.text = player.GetOVR().ToString();
+            newButton.GetComponentInChildren<Button>().onClick.AddListener(() => AddPlayerToTeam(player));
+        }
+        
+    }
+    public void AddPlayerToTeam(Player player)
+    {
+        print("added");
+        // Add player to the current team and update UI
+        Team currentTeam = t_Teams[t_currentTeamIndex];
+
+        if (currentTeam.AddPlayerToTeam(player))
+        {
+            p_DraftPlayerList.Remove(player);// Remove player from draft pool
+
+            // Remove the player's button from the UI and dictionary
+            if (playerButtonMap.ContainsKey(player))
+            {
+                GameObject playerButton = playerButtonMap[player];
+                Destroy(playerButton); // Remove the button from the UI
+                playerButtonMap.Remove(player); // Remove from dictionary
+            }
+
+            // Logic for advancing to the next team and any other updates
+            AdvanceToNextTeam();
+            
+        }
+        else
+        {
+            Debug.Log("This team is full, cannot add more players.");
+        }
+    }
+    void AdvanceToNextTeam()
+    {
+        t_currentTeamIndex++;
+
+        if (t_currentTeamIndex >= t_Teams.Count)
+        {
+            t_currentTeamIndex = 0; // Loop back to the first team
         }
     }
     public void BeginDraft()
--- Assets/Scripts/Team/Team.cs
+++ Assets/Scripts/Team/Team.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Team : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] string t_teamName;
+    [SerializeField] int t_currentPlayerOnTheTeamIndex = 0;
+    [SerializeField] int t_maxNumberOfPlayerOnTeam = 12;
+    [SerializeField] List<Player> t_players = new List<Player>();
+
+    public bool AddPlayerToTeam(Player newPlayer)
     {
-        
+        if (t_currentPlayerOnTheTeamIndex < t_maxNumberOfPlayerOnTeam)
+        {
+            t_players.Add(newPlayer);
+            t_currentPlayerOnTheTeamIndex++;
+            return true; // Player added successfully
+        }
+        else
+        {
+            Debug.Log("Team is full!");
+            return false; // Team is full, player not added
+        }
     }
 }
